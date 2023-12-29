98d892dc5bada65fb753bc1fe2d8e8653f1c52fc
Taha Shakir <tahashakir40@gmail.com>
2023-12-29 21:46:02 +0500

    Adding lambda function

---
+++ app.py
@@ -0,0 +1,21 @@
+import json
+
+def lambda_handler(event, context):
+    try:
+        parameter1 = int(event['p1'])
+        parameter2 = int(event['p2'])
+    except KeyError as e:
+        return {
+            'statusCode': 400,
+            'body': json.dumps(f'Missing required parameter: {str(e)}')
+        }
+    except ValueError:
+        return {
+            'statusCode': 400,
+            'body': json.dumps('Invalid parameter value. Please provide valid integers.')
+        }
+    result = parameter1 + parameter2
+    return {
+        'statusCode': 200,
+        'body': json.dumps({'result': result})
+    }
